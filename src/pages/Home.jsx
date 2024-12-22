da9294a03de5a97fc855ff8061b9a36856ea080d
Indireshvar-proj <v.indireshvaremail@gmail.com>
2024-12-22 14:24:57 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { Navbar, Main, Product, Footer,} from "../components";
+
+function Home() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Main />
+      
+      <Product />
+      <Footer />
+      
+    </>
+  )
+}
+
+export default Home
